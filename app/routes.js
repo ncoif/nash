04880c179f87764c3e0894b60cc9443bb63d93ea
ncoif <ncoif@gmx.com>
2014-03-05 01:57:08 +0000

    [bugfix] fix a deconection problem when rendering the root page.

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,8 @@ module.exports = function(app, passport) {
 	// show the home page (will also have our login links)
 	app.get('/', function(req, res) {
 		res.render('index.jade', {
-            title: "Index"
+            title: "Index",
+            user: req.isAuthenticated() ? req.user : null
         });
 	});
 
